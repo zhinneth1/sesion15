5ab20e1e7e0c004bd9d60fea577429fc3682422f
zhinneth1 <zhinneth.angulo.ttc.461@unilibre.edu.co>
2024-11-16 21:08:06 -0500

    css

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.getElementById('nav-toggle'),addEventListener('click', function(){
     document.querySelector('nav').classList.toggle('nav-open');
 });
+function Expectativas(texto){
+    alert(text);
+}
+function Capitulos(texto){
+    alert(text);
+}
+function Servidores(texto){
+    alert(text);
+}
+function Expectativas(texto){
+    alert(text);
+}
+function Recuerda(texto){
+    alert(text);
+}
